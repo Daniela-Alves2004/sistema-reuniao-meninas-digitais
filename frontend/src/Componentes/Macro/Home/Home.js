945b8c8fe698a1c63214bf297e18ef28c2a24665
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-01-18 02:00:48 -0300

    feat: add Home component and update Login styles for improved layout

---
+++ frontend/src/Componentes/Macro/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+
+  return (
+
+    <div className="divHome">
+
+        <h1>Home</h1>
+
+    </div>
+
+  );
+
+};
+
+export default Home;
