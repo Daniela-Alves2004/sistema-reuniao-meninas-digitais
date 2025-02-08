84099fd4a5d1dfa07c639ceb88ae67ba34292733
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-02-08 01:20:19 -0300

    feat: add Dockerfile and docker-compose for backend and frontend; include robots.txt, build assets, and update database configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,21 +4,28 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-
   process.env.POSTGRES_DB, // Nome do banco de dados
-
   process.env.POSTGRES_USER, // Usuário
-
   process.env.POSTGRES_PASSWORD, // Senha
-
   {
-
-    host: 'localhost', // Host
-    
+    host: process.env.POSTGRES_HOST, // Host
     dialect: 'postgres', // Dialeto
-
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    port: 5432 // Certifique-se de que a porta está correta
   }
-
 );
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
 
 module.exports = sequelize;
